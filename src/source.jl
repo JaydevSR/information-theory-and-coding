f5c8a014b5db6d9af14d5101a9b3c775d42ab4e4
Jaydev Singh Rao <59474411+JaydevSR@users.noreply.github.com>
2023-03-11 15:26:17 +0530

    Huffman encoder and Shannon-Fano-Elias encoder

--- src/source.jl
+++ src/source.jl
@@ -2,21 +2,24 @@ abstract type InformationSource end
 
 struct DiscreteMemorylessSource{T} <: InformationSource where T
     symbols::Vector{T}
+    nsymbols::Int
     distribution::Dict{T, Float64}
 end
 
 function DiscreteMemorylessSource(
                     distribution::Vector{Float64};
-                    symbols::Vector=[],
+                    symbols::Vector=collect(1:length(distribution)),
                     normalize=true)
-    n = length(distribution)
-    isnothing(symbols) ? symbols = collect(1:n) : 0
+    nsymbols = length(distribution)
+    isnothing(symbols) ? symbols = collect(1:nsymbols) : 0
     normalize ? distribution /= sum(distribution) : 0
 
     T = eltype(symbols)
     return DiscreteMemorylessSource{T}(
         symbols,
-        Dict(symbols[i] => distribution[i] for i in 1:n))
+        nsymbols,
+        Dict(symbols[i] => distribution[i] for i in 1:nsymbols))
 end
 
 entropy(X::DiscreteMemorylessSource) = entropy(dms.distribution)
+Base.length(source::DiscreteMemorylessSource) = source.nsymbols
--- src/source_coding.jl
+++ src/source_coding.jl
@@ -5,26 +5,58 @@ Check if the given vector of codeword lengths satisfies the Kraft inequality.
 """
 is_kraft_compatible(L::Vector{Integer}; base=2) = (sum(base .^ -L) <= 1)
 
-struct HuffmanEndcoder{T}
-    alphabet::Vector{T}
-    alphabet_size::Integer
+struct HuffmanEncoder end
+
+function encode(source::DiscreteMemorylessSource, encoder::HuffmanEncoder)
+    sorted_symbols = sort(source.symbols, by=(x -> source.distribution[x]), rev=true)
+    codes = source.distribution |> values |> collect |> x -> encode(x, encoder)
+    return Dict(zip(sorted_symbols, codes))
 end
 
-function HuffmanEndcoder(alphabet::Vector{T}) where T
-    alphabet_size = length(alphabet)
-    return HuffmanEndcoder{T}(alphabet, alphabet_size)
+function encode(distribution::Vector{Float64}, encoder::HuffmanEncoder)
+    if length(distribution) == 1
+        return ["0"]
+    elseif length(distribution) == 2
+        return ["0", "1"]
+    else
+        nsymbols = length(distribution)
+        code = ones(String, nsymbols)
+        sort!(distribution, rev=true)
+
+        mintwo_merged = distribution[end-1] + distribution[end]
+        pass_distribution = push!(distribution[1:nsymbols-2], mintwo_merged)
+        subcode = encode(pass_distribution, encoder)
+
+        code[1:end-2] = subcode[1:end-1]
+        code[end-1] = subcode[end]*"0"
+        code[end] = subcode[end]*"1"
+        return code
+    end
 end
 
-HuffmanEndcoder() = HuffmanEndcoder([true, false], 2)
+struct ShannonFanoEliasEncoder end
 
-function encode(source, encoder::HuffmanEndcoder)
-    coding = Dict(source.alphabet[i] => "" for i in 1:source.alphabet_size)
+function encode(source::DiscreteMemorylessSource, encoder::ShannonFanoEliasEncoder)
+    sorted_symbols = sort(source.symbols, by=(x -> source.distribution[x]), rev=true)
+    codes = source.distribution |> values |> collect |> x -> encode(x, encoder)
+    return Dict(zip(sorted_symbols, codes))
+end
 
-    # Sort the alphabet by probability
-    sorted_alphabet = sort(source.alphabet, by=(i -> source.distribution[i]))
-    sorted_distribution = [source.distribution[i] for i in sorted_alphabet]
+function encode(distribution::Vector{Float64}, encoder::ShannonFanoEliasEncoder)
+    nsymbols = length(distribution)
+    sort!(distribution, rev=true)
+    cdf = cumsum(distribution)
+    modified_cdf = (distribution/2) .+ [0; view(cdf, 1:nsymbols-1)]
+    trunc_lengths = ceil.(log.(inv.(distribution))) .+ 1
 
-    while length(sorted_distribution) > 1
-        # TODO
+    codes = [binary_decimal(p, l) for (p, l) in zip(modified_cdf, trunc_lengths)] 
+end
+
+function binary_decimal(d, l, head="")
+    if l == 0
+        return head
+    else
+        d *= 2
+        d >= 1 ? binary_decimal(d - 1, l - 1, head*"1") : binary_decimal(d, l - 1, head*"0")
     end
 end
